0549f415fdb03b25dcec7677efe9c4950a7220bf
Adrian Clement <adrianwclement@gmail.com>
2024-08-11 20:01:48 +0200

    refactored app layout

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -1,34 +1,35 @@
-"use client";
+// "use client";
 
-import { useState, ChangeEvent } from "react";
+// import { useState, useEffect, ChangeEvent } from "react";
 
-import styles from "../styles/GameBoard.module.css";
+// import styles from "../styles/GameBoard.module.css";
+// import { decodeMessage, isCorrectDecode } from "../lib/gameLogic";
 
-export const GameBoard = () => {
-  const [input, setInput] = useState("");
-  const [decodedMessage, setDecodedMessage] = useState("");
+// export const GameBoard = () => {
+//   const [input, setInput] = useState("");
+//   const [decodedMessage, setDecodedMessage] = useState("");
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
-  };
+//   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     setInput(event.target.value);
+//   };
 
-  const handleDecode = () => {
-    setDecodedMessage(`Decoded: ${input}`);
-  };
+//   const handleDecode = () => {
+//     setDecodedMessage(`Decoded: ${input}`);
+//   };
 
-  return (
-    <div className={styles.gameBoard}>
-      <input
-        className={styles.gameInput}
-        type="text"
-        value={input}
-        onChange={handleInputChange}
-        placeholder="Enter your guess"
-      />
-      <button className={styles.gameButton} onClick={handleDecode}>
-        Decode
-      </button>
-      <p>{decodedMessage}</p>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.gameBoard}>
+//       <input
+//         className={styles.gameInput}
+//         type="text"
+//         value={input}
+//         onChange={handleInputChange}
+//         placeholder="Enter your guess"
+//       />
+//       <button className={styles.gameButton} onClick={handleDecode}>
+//         Decode
+//       </button>
+//       <p>{decodedMessage}</p>
+//     </div>
+//   );
+// };
