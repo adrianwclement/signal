8ded2d772aba3864f3e88367cc3453167601f8a9
Adrian Clement <adrianwclement@gmail.com>
2024-08-11 20:01:11 +0200

    set new API endpoints for accessing postgres db

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,9 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
+export const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: parseInt(process.env.PGPORT || "5432", 10),
 });
-
-export default pool;
